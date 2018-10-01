0d735fd2117d40f67e0b3da05cdc380c0a88c6a4
Derek <d.schlieps@gmail.com>
2018-10-01 02:47:54 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/products", function(req, res){
+    res.render("products");
+});
+
+app.get("/services", function(req, res){
+    res.render("services");
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("HollowLog Server Has Started");
+});
