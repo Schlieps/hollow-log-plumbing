f612141d586e869a065d235c004a44a18fd80787
Derek <d.schlieps@gmail.com>
2018-10-22 03:08:06 +0000

    commit all files for v2 of website

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+ // preloader
+    window.addEventListener('load', () => document.querySelector('.preloader').classList.add('hidePreloader'));
+
+$(document).ready(function(){
+    // magnific popup
+    $('.parent-container').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery:{
+        enabled:true
+      }
+      // other options
+    });
+    
+    // init Isotope
+    var $grid = $('.grid').isotope({
+      // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on( 'click', 'button', function() {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
+    });
+    // layout Isotope after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.isotope('layout');
+    });
+});
